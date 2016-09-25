dff9af9a6e9bd77e02d5993cb0e678651eec7d7b
Nartallax <nartallax@gmail.com>
2016-09-25 22:57:49 +0300

    latest changes

--- script/commons/cliargs.js
+++ script/commons/cliargs.js
@@ -1,8 +1,5 @@
 var CLIArgs = (() => {
-	var argArr = Array.prototype.slice.call(process.argv, 2);
-	
 	var argFail = msg => fail(msg, 'ArgumentParsingError');
-	var argFailAt = (msg, index) => argFail(msg + ' "' + argArr[index] + '".')
 	var badNameFail = (name, msg) => argFail('Incorrect parameter name "' + name + '": ' + msg + '.');
 	
 	var namesOf = names => {
@@ -33,98 +30,102 @@ var CLIArgs = (() => {
 			return names;
 		}
 		
-		badNameFail('string expected')
-	}
-		
-	var indicesOf = names => {
-		var filter = {};
-		names.forEach(n => filter[n] = true);
-		
-		return argArr
-			.map((a, i) => ({arg: a, index: i}))
-			.filter(arg => filter[arg.arg])
-			.map(arg => arg.index);
+		badNameFail('string or array of string expected, got ' + typeof(names))
 	}
 	
-	var oneOrZeroIndexOf = names => {
-		var indices = indicesOf(names);
-		indices.length < 2 || argFail('Expected no more than one of "' + names.join('", "') + '" at time.');
-		return indices.length > 0? indices[0]: null;
-	}
+	var typeAliases = {
+		'double': 'double',
+		'float': 'double',
+		'number': 'double',
+		'dbl': 'double',
+		'flt': 'double',
+		'num': 'double',
 		
-	var oneIndexOf = names => {
-		var index = oneOrZeroIndexOf(names);
-		index !== null || argFail('Expected one argument of "' + names.join('", "') + '".'); 
-		return index;
+		'int': 'int',
+		'lng': 'int',
+		'long': 'int',
+		
+		'string': 'string',
+		'str': 'string',
+		
+		'boolean': 'bool',
+		'bool': 'bool'
 	}
 	
-	var nonLastIndicesOf = names => {
-		var indices = indicesOf(names);
-		indices.foreach(i => argArr.length > index + 1 || argFailAt('Expected value after', index))
-		return indices;
+	var getArgValueByDescription = (argArr, names, rawDesc) => {
+		var isArray = rawDesc.type.endsWith('s');
+		var realType = typeAliases[rawDesc.type.replace(/s$/, '')];
+		
+		if(!realType) argFail('"' + rawDesc.type + '" is not known type name.');
+		if(realType === 'bool' && isArray) argFail('Boolean flags could not come in arrays.');
+		if(realType === 'string' && isArray) argFail('Strings could not come in arrays.');
+		
+		var haveDefault = 'default' in rawDesc;
+		var index = indexOf(names);
+		
+		if(index === null){
+			haveDefault || argFail('Required at least one option of ' + names);
+			return rawDesc.default;
+		} else {
+			if(realType === 'bool') return true;
+			
+			(index === argArr.length - 1) && argFail('Expected some value after ' + argArr[index]);
+			
+			var rawValue = argArr[index];
+			
+			if(realType === 'string') return rawValue;
+			
+			var values = (isArray? rawValue.split(/,/).filter(p => p): [rawValue]).map(v => {
+				if(realType === 'int'){
+					v.match(/^\d+$/) || argFail('Expected integer after ' + argArr[index] + ', got "' + v + '" instead.');
+					return parseInt(v, 10);
+				} else {
+					v.match(/^\d+(\.\d+)?$/) || argFail('Expected number after ' + argArr[index] + ', got "' + v + '" instead.');
+					return parseFloat(v, 10);
+				}
+			});
+			
+			return isArray? values: values[0];
+		}
 	}
 	
-	var oneOrZeroNonLastIndexOf = names => {
-		var index = oneOrZeroIndexOf(names);
-		index === null || argArr.length > index + 1 || argFailAt('Expected value after', index);
-		return index;
-	}
 	
-	var oneNonLastIndexOf = names => {
-		var index = oneIndexOf(names);
-		argArr.length > index + 1 || argFailAt('Expected value after', index);
-		return index;
-	}
+	var parseArgsByDescription = (argArr, description) => {
+		var result = {};
+		var usedNames = {};
 		
-	var have = names => indicesOf(names).length > 0
-	var haveOne = names => oneOrZeroIndexOf(names) === null? false: true
+		Object.keys(description).foreach(rawName => {
+			var names = namesOf(rawName);
+			var v = getArgValueByDescription(argArr, names, description[rawName]);
+			names.foreach(n => {
+				(n in result) && argFail('Duplicate argument key: ' + n);
+				result[n] = v;
+			});
+		})
+	};
 	
-/*	
-	var oneValueAfter = names => {
-		var index = oneIndexOf(names);
-		if(argArr.length <= index + 1) 
-		return argArr[index + 1];
-	}
+	var showHelpByDescription = description => {
+		var helpString = Object.keys(description).map(k => {
+			var desc = 'description' in description[k]? '\n' + description[k].description: '';
+			return '\t' + k + '\n' + desc;
+		}).mkString('\n');
 		
-	var oneOrZeroValueAfter = names => {
-		var index = oneOrZeroIndexOf(names);
-		if(index === null) return null;
-		if(argArr.length <= index + 1) fail('Expected value after "' + argArr[index] + '".', 'ArgumentParsingError');
-		return argArr[index + 1];
+		console.error(helpString);
 	}
-		
-	var valuesAfter: names => indicesOf(names).map(i => {
-		if(argArr.length <= i + 1) fail('Expected value after "' + argArr[i] + '".', 'ArgumentParsingError');
-		return argArr[i + 1];
-	})
-*/
 
 /*
-
 {
 	'-p, --param-name-a': { type: 'int', default: 5, description: 'This is free-form description.'}
 }
-
 */
-
-	var typeReaders = {
-		'bool': n => haveOne(n)? true: false,
-		'int': (n, deflt) => {
-			var i = oneOrZeroNonLastIndexOf(n)
-			if(i === null) return null;
-			var v = argArr[i + 1];
-			v.match(/^\-?\d+$/) || fail('Expected integer number after ' + argArr[])
-		}
-	}
-	var bool = 
-
-	var args = {
-		bool: names => haveOne(names),
-		int: names => {
-			
-		}
-	};
 	
-	return args;
+	var CLI = function(desc, args){ this.desc = desc; this.args = args || Array.prototype.slice.call(process.argv, 2) }
+	
+	CLI.prototype = {
+		values: function(){ return parseArgsByDescription(this.args, this.desc) },
+		showHelp: function(){ return showHelpByDescription(this.desc) }
+	}
+	
+	return CLI;
 	
 })();
